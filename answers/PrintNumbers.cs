da1900d3438753abf841198500eb6fe78bae9390
yoni2z <zyonas626@gmail.com>
2024-04-05 15:01:07 +0300

    Answers

---
+++ answers/PrintNumbers.cs
@@ -0,0 +1,7 @@
+class PrintNumbers{
+    public void print(){
+         for (int i=10; i<=100; i+=10){
+        Console.WriteLine(i);
+        }
+    }
+}
---
+++ answers/PrintString.cs
@@ -0,0 +1,7 @@
+class PrintString{
+    String gameProject = "This is a game project";
+    public void print(){
+        Console.WriteLine(gameProject);
+    }
+}
+
